a888bba5d9b8fa21322a5103dbd48b331edf105f
GoonerMAK <mashrurahsan@iut-dhaka.edu>
2023-09-07 10:19:19 +0600

    Lab_04 (Factory Method)
    - Factory Method
    - Abstract Factory Method

---
+++ Lab_04/PizzaDrive.java
@@ -0,0 +1,197 @@
+import java.util.ArrayList;
+
+// Abstract Pizza class
+abstract class Pizza {
+    String name;
+    String dough;
+    String sauce;
+    ArrayList toppings = new ArrayList();
+    void prepare() {
+        System.out.println("Preparing " + name);
+        System.out.println("Tossing dough...");
+        System.out.println("Adding sauce...");
+        System.out.println("Adding toppings: ");
+        for (int i = 0; i < toppings.size(); i++) {
+            System.out.println(" " + toppings.get(i));
+        }
+    }
+    void bake() {
+        System.out.println("Bake for 25 minutes at 350");
+    }
+    void cut() {
+        System.out.println("Cutting the pizza into diagonal slices");
+    }
+    void box() {
+        System.out.println("Place pizza in official PizzaStore box\n");
+    }
+    public String getName() {
+        return name;
+    }
+}
+
+// NY Style Cheese Pizza
+class NYStyleCheesePizza extends Pizza {
+    public NYStyleCheesePizza() {
+        name = "NY Style Sauce and Cheese Pizza";
+        dough = "Thin Crust Dough";
+        sauce = "Marinara Sauce";
+        toppings.add("Grated Reggiano Cheese");
+    }
+}
+
+// NY Style Veggie Pizza
+class NYStyleVeggiePizza extends Pizza {
+    public NYStyleVeggiePizza() {
+        name = "NY Style Veggie Pizza";
+        dough = "Thin Crust Dough";
+        sauce = "Marinara Sauce";
+        toppings.add("Grated Reggiano Cheese");
+        toppings.add("Bell Peppers");
+        toppings.add("Onions");
+        toppings.add("Mushrooms");
+    }
+}
+
+// NY Style Clam Pizza
+class NYStyleClamPizza extends Pizza {
+    public NYStyleClamPizza() {
+        name = "NY Style Clam Pizza";
+        dough = "Thin Crust Dough";
+        sauce = "Marinara Sauce";
+        toppings.add("Grated Reggiano Cheese");
+        toppings.add("Fresh Clams");
+        toppings.add("Garlic");
+        toppings.add("Oregano");
+    }
+}
+
+// NY Style Pepperoni Pizza
+class NYStylePepperoniPizza extends Pizza {
+    public NYStylePepperoniPizza() {
+        name = "NY Style Pepperoni Pizza";
+        dough = "Thin Crust Dough";
+        sauce = "Marinara Sauce";
+        toppings.add("Grated Reggiano Cheese");
+        toppings.add("Pepperoni");
+        toppings.add("Onions");
+        toppings.add("Green Peppers");
+    }
+}
+
+// Chicago Style Cheese Pizza
+class ChicagoStyleCheesePizza extends Pizza {
+    public ChicagoStyleCheesePizza() {
+        name = "Chicago Style Deep Dish Cheese Pizza";
+        dough = "Extra Thick Crust Dough";
+        sauce = "Plum Tomato Sauce";
+        toppings.add("Shredded Mozzarella Cheese");
+    }
+    void cut() {
+        System.out.println("Cutting the pizza into square slices");
+    }
+}
+
+// Chicago Style Veggie Pizza
+class ChicagoStyleVeggiePizza extends Pizza {
+    public ChicagoStyleVeggiePizza() {
+        name = "Chicago Style Deep Dish Veggie Pizza";
+        dough = "Extra Thick Crust Dough";
+        sauce = "Plum Tomato Sauce";
+        toppings.add("Shredded Mozzarella Cheese");
+        toppings.add("Bell Peppers");
+        toppings.add("Onions");
+        toppings.add("Mushrooms");
+    }
+}
+
+// Chicago Style Clam Pizza
+class ChicagoStyleClamPizza extends Pizza {
+    public ChicagoStyleClamPizza() {
+        name = "Chicago Style Clam Pizza";
+        dough = "Extra Thick Crust Dough";
+        sauce = "Plum Tomato Sauce";
+        toppings.add("Fresh Clams");
+        toppings.add("Garlic");
+        toppings.add("Oregano");
+    }
+}
+
+// Chicago Style Pepperoni Pizza
+class ChicagoStylePepperoniPizza extends Pizza {
+    public ChicagoStylePepperoniPizza() {
+        name = "Chicago Style Pepperoni Pizza";
+        dough = "Extra Thick Crust Dough";
+        sauce = "Plum Tomato Sauce";
+        toppings.add("Shredded Mozzarella Cheese");
+        toppings.add("Pepperoni");
+        toppings.add("Onions");
+        toppings.add("Green Peppers");
+    }
+}
+
+
+// Abstract PizzaStore class
+abstract class PizzaStore {
+    public Pizza orderPizza(String type) {
+        Pizza pizza = createPizza(type);
+
+        pizza.prepare();
+        pizza.bake();
+        pizza.cut();
+        pizza.box();
+
+        return pizza;
+    }
+
+    protected abstract Pizza createPizza(String type);
+}
+
+// NY PizzaStore
+class NYPizzaStore extends PizzaStore {
+    @Override
+    protected Pizza createPizza(String item) {
+        if (item.equals("cheese")) {
+            return new NYStyleCheesePizza();
+        } else if (item.equals("veggie")) {
+            return new NYStyleVeggiePizza();
+        } else if (item.equals("clam")) {
+            return new NYStyleClamPizza();
+        } else if (item.equals("pepperoni")) {
+            return new NYStylePepperoniPizza();
+        }
+        return null;
+    }
+}
+
+// Chicago PizzaStore
+class ChicagoPizzaStore extends PizzaStore {
+    @Override
+    protected Pizza createPizza(String item) {
+        if (item.equals("cheese")) {
+            return new ChicagoStyleCheesePizza();
+        } else if (item.equals("veggie")) {
+            return new ChicagoStyleVeggiePizza();
+        } else if (item.equals("clam")) {
+            return new ChicagoStyleClamPizza();
+        } else if (item.equals("pepperoni")) {
+            return new ChicagoStylePepperoniPizza();
+        }
+        return null;
+    }
+}
+
+// Main class to demonstrate
+public class PizzaDrive {
+    public static void main(String[] args) {
+        PizzaStore nyStore = new NYPizzaStore();
+        PizzaStore chicagoStore = new ChicagoPizzaStore();
+
+        Pizza pizza = nyStore.orderPizza("cheese");
+
+        pizza = chicagoStore.orderPizza("cheese");
+
+        pizza = nyStore.orderPizza("veggie");
+
+        pizza = chicagoStore.orderPizza("clam");
+    }
+}
